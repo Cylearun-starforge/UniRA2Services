860faed1d5c7cb64482a019392da2f140eb804cd
kaixa <690750353@qq.com>
2022-09-03 23:52:04 +0800

    feat: init

---
+++ HttpServerTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using HttpServer.Controllers;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace HttpServerTests.Controllers
+{
+	[TestClass()]
+	public class WeatherForecastControllerTests
+	{
+		[TestMethod()]
+		public void GreetingTest()
+		{
+			var logger = new Mock<ILogger<WeatherForecastController>>();
+			var controller = new WeatherForecastController(logger.Object);
+			var greeting = controller.Greeting();
+			Assert.AreEqual("Hello, World!", greeting);
+			Assert.AreEqual(1, logger.Invocations.Count);
+			Assert.AreEqual("Log", logger.Invocations[0].Method.Name);
+		}
+	}
+}
